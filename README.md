71cd0f5fba81c616535ebe2af79bff10ca44afcf
GwiHwan Go <83144588+GwiHwan-Go@users.noreply.github.com>
2022-05-08 01:31:27 +0900

    Edit names

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # C_study
+
+From the course AAI2011(Lee. C) in SKKU 2022 Spring Semester
--- hw1-1/intro1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    
-    printf("인공지능융합전공\n2018314374\n고귀환");
-
-    return 0;
-    
-}
